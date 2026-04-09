7b6b1df2fac5ceef961cb4c36416e272cdd73713
bit-nebula <bit-nebula@outlook.com>
2026-04-09 10:43:52 +0800

    添加Hosting扩展

---
+++ src/DevelopKit/Hosting/CrossPlatformHostingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Nebula.DevelopKit.Web;
+
+public static class CrossPlatformHostingExtensions
+{
+    public static IServiceCollection AddCrossPlatformHostingService(this IServiceCollection services)
+    {
+        return AddCrossPlatformHostingService(services, string.Empty);
+    }
+
+    public static IServiceCollection AddCrossPlatformHostingService(this IServiceCollection services, string serviceName)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            services.AddWindowsService(options =>
+            {
+                if (!string.IsNullOrEmpty(serviceName))
+                {
+                    options.ServiceName = serviceName;
+                }
+            });
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            services.AddSystemd();
+        }
+        return services;
+    }
+}
