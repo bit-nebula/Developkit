a7e1f6e0a1245d3948efb8002a6dc909649c2ac2
bit-nebula <bit-nebula@outlook.com>
2026-04-28 15:16:31 +0800

    add: 增加抽象基础实体

---
+++ src/DevelopKit/Entity/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace BitNebula.DevelopKit.Entity;
+
+public abstract class Entity : IEntity<long>
+{
+    public long Id { get; set; }
+
+    public DateTime CreateTime { get; set; }
+
+    public DateTime UpdateTime { get; set; }
+}
---
+++ src/DevelopKit/Entity/IEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace BitNebula.DevelopKit.Entity;
+
+public interface IEntity<T> where T : IEquatable<T>
+{
+    /// <summary>
+    /// 主键
+    /// </summary>
+    T Id { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    DateTime CreateTime { get; set; }
+
+    /// <summary>
+    /// 更新时间
+    /// </summary>
+    DateTime UpdateTime { get; set; }
+}
